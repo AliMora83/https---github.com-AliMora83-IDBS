719fc205da069078065362972246f50e169655a1
Daurius Swarts <ali@openmindi.co.za>
2022-05-25 21:56:50 +0200

    Init Commit

--- src/idbs_assets/src/index.js
+++ src/idbs_assets/src/index.js
@@ -1,19 +1,39 @@
 import { idbs } from "../../declarations/idbs";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
-
-  const name = document.getElementById("name").value.toString();
-
-  button.setAttribute("disabled", true);
-
-  // Interact with foo actor, calling the greet method
-  const greeting = await idbs.greet(name);
-
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
+window.addEventListener("load", async function(){
+  // console.log("Finished loading Boss");
+update();
 });
+
+document.querySelector("form").addEventListener("submit", async function(event) {
+  event.preventDefault();
+  // console.log("Submitted Boss");
+
+    const button = event.target.querySelector("#submit-btn");
+
+    const inputAmount = parseFloat(document.getElementById("input-amount").value);
+    const outputAmount = parseFloat(document.getElementById("withdrawal-amount").value);
+
+    button.setAttribute("disabled", true);
+
+    if (document.getElementById("input-amount").value.length != 0) {
+      await idbs.topUp(inputAmount);
+    }
+
+    if (document.getElementById("withdrawal-amount").value.length != 0) {
+      await idbs.withdraw(outputAmount);
+    }
+
+    await idbs.compound();
+
+    update();
+    document.getElementById("input-amount").value = "";
+    document.getElementById("withdrawal-amount").value = "";
+
+    //button.removeAtrribute("disabled");
+});
+
+async function update() {
+  const currentAmount = await idbs.checkBalance();
+  document.getElementById("value").innerText = Math.round(currentAmount * 100) / 100;
+};
